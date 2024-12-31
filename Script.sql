1f3837a4e75acbbadd0ae28ffb79060698ffa4df
Iyad Yousef <iyad.yousef04@gmail.com>
2024-12-31 12:04:58 +0800

    Committing DBEAVER codes

---
+++ Script.sql
@@ -0,0 +1,80 @@
+CREATE TABLE IF NOT EXISTS Buyer (
+            ID SERIAL UNIQUE NOT NULL PRIMARY KEY,
+            Name TEXT NOT NULL,
+            Address TEXT NOT NULL,
+            Contract_details INT NOT null,
+            CONSTRAINT buyer_unique_name UNIQUE (Name, Address) 
+        );
+
+
+CREATE TABLE IF NOT EXISTS Printer (
+            ID SERIAL UNIQUE NOT NULL PRIMARY KEY,
+            Name TEXT NOT NULL,
+            Address TEXT NOT NULL,
+            Contract_details INT NOT null,
+            CONSTRAINT printer_unique_name UNIQUE (Name, Address) 
+        );
+
+
+CREATE TABLE IF NOT EXISTS Product (
+            ID SERIAL UNIQUE NOT NULL PRIMARY KEY,
+            Item_desc TEXT NOT null,
+            CONSTRAINT product_unique_name UNIQUE (Item_desc) 
+
+        );
+
+CREATE TABLE IF NOT EXISTS Supplier (
+            ID SERIAL UNIQUE NOT NULL PRIMARY KEY,
+            Name TEXT NOT NULL,
+            Address TEXT NOT NULL,
+            Contract_details INT NOT NULL,
+            CONSTRAINT supplier_unique_name UNIQUE (Name, Address) 
+
+        );
+
+CREATE TABLE IF NOT EXISTS Agent (
+            ID SERIAL UNIQUE NOT NULL PRIMARY KEY,
+            Name TEXT NOT NULL,
+            Address TEXT NOT NULL,
+            Contract_details INT NOT NULL,
+            CONSTRAINT agent_unique_name UNIQUE (Name, Address) 
+
+        );
+
+CREATE TABLE IF NOT EXISTS Invoice (
+            ID SERIAL UNIQUE NOT NULL PRIMARY KEY,
+            Payment_request_num TEXT NOT NULL,
+            Delivery_client_num TEXT NOT NULL,
+            Billing_num TEXT NOT NULL,
+            Collection_num TEXT NOT NULL,
+            Deposit_cheque_num TEXT NOT NULL,
+            Status TEXT NOT NULL CHECK(Status IN ('Approved', 'Disapproved'))
+        );
+
+CREATE TABLE IF NOT EXISTS Date (
+            ID SERIAL UNIQUE NOT NULL PRIMARY KEY,
+            Year INT NOT NULL,
+            Quarter INT NOT NULL,
+            Month INT NOT NULL,
+            Day INT NOT NULL
+        );
+
+CREATE TABLE IF NOT EXISTS Revenue (
+            ID SERIAL UNIQUE NOT NULL PRIMARY KEY,
+            ID_buyer INT NOT NULL REFERENCES Buyer(ID),
+            ID_printer INT NOT NULL REFERENCES Printer(ID),
+            ID_product INT NOT NULL REFERENCES Product(ID),
+            ID_agent INT NOT NULL REFERENCES Agent(ID),
+            ID_supplier INT NOT NULL REFERENCES Supplier(ID),
+            ID_invoice INT NOT NULL REFERENCES Invoice(ID),
+            ID_date INT NOT NULL REFERENCES Date(ID),
+            Price_per_item REAL NOT NULL,
+            Cost_per_item REAL NOT NULL,
+            Print_per_item REAL NOT NULL,
+            Commission_rate REAL NOT NULL,
+            Production_start_date INT NOT NULL,
+            Delivery_date INT NOT NULL,
+            Billing_date INT NOT NULL,
+            Payment_date INT NOT NULL
+        )
+  
